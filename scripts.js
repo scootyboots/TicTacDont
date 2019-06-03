e9f5b60173fbcff65990e9857290c2bd828c4c48
scootyboots <scott.coffrin@gmail.com>
2019-06-03 23:40:49 +0900

    add animations for game reset and small changes to init function

--- scripts.js
+++ scripts.js
@@ -1,140 +1,3 @@
-/**
- * var firstPlayer = true
-
-var box1_1 = document.getElementById('box1-1')
-var box1_2 = document.getElementById('box1-2')
-var box1_3 = document.getElementById('box1-3')
-
-var box2_1 = document.getElementById('box2-1')
-var box2_2 = document.getElementById('box2-2')
-var box2_3 = document.getElementById('box2-3')
-
-var box3_1 = document.getElementById('box3-1')
-var box3_2 = document.getElementById('box3-2')
-var box3_3 = document.getElementById('box3-3')
-
-
-var allBoxes = [box1_1, box1_2, box1_3, box2_1, box2_2, box2_3, box3_1, box3_2, box3_3]
-
-init();
-
-allBoxes.map(function(box) {
-
-    box.addEventListener('click', (event) => {changeLetter(box)})
-
-});
-
-
-function togglePlayer() {
-
-    if (firstPlayer === true) {
-        firstPlayer = false;
-    } else {
-        firstPlayer = true;
-    }
-}
-
-
-function changeLetter(box) {
-
-    if (firstPlayer === true) {
-
-        box.classList.toggle('blue');
-
-        box.innerHTML = 'x'
-
-    } else {
-        
-        box.classList.toggle('red');
-
-        box.innerHTML = 'o'
-    }
-
-    document.querySelector('span.player1').classList.toggle('active');
-    document.querySelector('span.player2').classList.toggle('active');
-
-    checkWin();
-    togglePlayer();
-
-};
-
-var rowWin1 = [box1_1, box1_2, box1_3]
-var rowWin2 = [box2_1, box2_2, box2_3]
-var rowWin3 = [box3_1, box3_2, box3_3]
-
-var colWin1 = [box1_1, box2_1, box3_1]
-var colWin2 = [box1_2, box2_2, box3_2]
-var colWin3 = [box1_3, box2_3, box3_3]
-
-var diagWin1 = [box1_1, box2_2, box3_3]
-var diagWin2 = [box1_3, box2_2, box3_1]
-
-var allWinConditions = [rowWin1, rowWin2, rowWin3, colWin1, colWin2, colWin3, diagWin1, diagWin2]
-
-function checkWin() {
-    
-    var gameWon = false
-
-    for (let i = 0; i < allWinConditions.length; i++) {
-
-        var winCondition = allWinConditions[i]
-        var box1 = winCondition[0].innerText;
-        var box2 = winCondition[1].innerText;
-        var box3 = winCondition[2].innerText;
-
-        if (box1 === box2 && box1 === box3){
-            gameWon = true;
-        } else {
-            continue;
-        }
-
-    }
-
-    if (gameWon === true) {
-        resetGame();
-    }
-
-};
-
-
-function resetGame() {
-
-    var winner = firstPlayer === true ? '1' : '2';
-
-    alert('Player ' + winner + ' has won! Play again?')
-
-    init();
-
-}
-
-
-
-function init() {
-
-    box1_1.innerText = 'a'
-    box1_2.innerText = 'b'
-    box1_3.innerText = 'c'
-
-    box2_1.innerText = 'd'
-    box2_2.innerText = 'e'
-    box2_3.innerText = 'f'
-
-    box3_1.innerText = 'g'
-    box3_2.innerText = 'h'
-    box3_3.innerText = 'k'
-
-    allBoxes.forEach(function(i) {
-        i.className = ''
-    });
-
-    document.querySelector('.player1').className = 'player1 active'
-    document.querySelector('.player2').className = 'player2'
-
-};
- */
-
-// var allCells = Array.from(document.querySelectorAll('div[class*="cell"]'))
-
 var firstPlayer = true
 
 // html element of each cell in the grid put into variables
@@ -211,10 +74,6 @@ function changeImg(cell) {
 
     }
 
-    // displays who the active player is 
-    // document.querySelector('span.player1').classList.toggle('active');
-    // document.querySelector('span.player2').classList.toggle('active');
-
     checkWin();
     togglePlayer();
 
@@ -226,7 +85,20 @@ function resetGame() {
 
     alert(`Player ${winner} has won! Play again?`)
 
-    init();
+    // applies animation to fade out existing moves 
+    cell_1_1.querySelector('img').classList.add('fade-out', 'hide-opacity', 'box-slide-tl');
+    cell_1_2.querySelector('img').classList.add('fade-out', 'hide-opacity', 'box-slide-top');
+    cell_1_3.querySelector('img').classList.add('fade-out', 'hide-opacity', 'box-slide-tr');
+
+    cell_2_1.querySelector('img').classList.add('fade-out', 'hide-opacity', 'box-slide-left');
+    cell_2_2.querySelector('img').classList.add('fade-out', 'hide-opacity');
+    cell_2_3.querySelector('img').classList.add('fade-out', 'hide-opacity', 'box-slide-right');
+
+    cell_3_1.querySelector('img').classList.add('fade-out', 'hide-opacity', 'box-slide-bl');
+    cell_3_2.querySelector('img').classList.add('fade-out', 'hide-opacity', 'box-slide-bottom');
+    cell_3_3.querySelector('img').classList.add('fade-out', 'hide-opacity', 'box-slide-br');
+
+    setTimeout(init, 450);
 
 }
 
@@ -285,8 +157,8 @@ function init() {
     }
 
     // resets animation classes on turn indicator circle
-    circle.classList.remove('slide-right')
-    circle.classList.remove('slide-left')
+    circle.classList.remove('slide-right', 'slide-left')
+    circle.classList.add('slide-left')
 
 }
 
